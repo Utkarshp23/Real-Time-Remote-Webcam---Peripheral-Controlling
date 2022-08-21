7a2ccf6f2ccd2bb2e778817c79be4b46e911676a
Utkarshp23 <utkarshpawar2332@gmail.com>
2022-08-21 18:57:36 +0530

    Finalized Project Structure

---
+++ Source Code/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get install python3-tk python3-dev
+pip3 install opencv-python
+pip3 install cv
+pip3 install pyautogui
+pip3 install binascii
+pip3 install pynput
+pip3 install pyfakewebcam
+apt install ffmpeg
+apt install v4l2loopback-dkms v4l2loopback-utils
+modprobe -r v4
